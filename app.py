3ac50a8562c40f65ea4df6d14cbd93a5abb87a68
Vital <x6vital@gmail.com>
2024-05-10 21:58:16 +0300

    Homework 10.05.2024

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_user_page():
         return 'User updated'
 
 
-@app.route('user/funds', methods=['GET', 'POST'])
+@app.route('/user/funds', methods=['GET', 'POST'])
 def get_funds_page():
     if request.method == 'GET':
         return 'Welcome to your funds page'

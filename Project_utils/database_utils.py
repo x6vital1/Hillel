a86e61378e6a9313f74bba2560fe1624d9ae4622
Vital <x6vital@gmail.com>
2024-05-26 01:48:24 +0300

    Homework 26.05.2024

---
+++ Project_utils/database_utils.py
@@ -0,0 +1,68 @@
+import sqlite3
+from typing import Dict, Optional, List, Any, Union, Tuple
+
+
+def dict_factory(cursor, row) -> Dict[str, Any]:
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+class SQLiteDatabase:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_path)
+        self.connection.row_factory = dict_factory
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
+
+    def select(self, table_name: str, method: str, conditions: Optional[Dict] = None, columns: Optional[List] = None,
+               join_conditions: Optional[Dict[str, Dict[str, Any]]] = None) -> Dict[str, Any]:
+        """
+        Выборка данных из базы данных
+        :param table_name: Название талицы. Например: 'users'
+        :param method: Метод выборки. Например: 'fetchall', 'fetchone'
+        :param conditions: Условия выборки. Например: {'id': 1}
+        :param columns: Колонки для выборки. Например: ['id', 'name']. Если используем JOIN и хотим поменять название
+         колонки, используем "AS". Например: ['users.id AS user_id', 'users.name AS user_name']
+        :param join_conditions: Соединение таблиц. Например: {'users': {'id': 'users.id'}}
+        :return:
+        """
+        conditions_list = []
+        join_conditions_list = []
+        if columns:
+            query = f'SELECT {", ".join(columns)} FROM {table_name}'
+        else:
+            query = f'SELECT * FROM {table_name}'
+        if join_conditions is not None:
+            for join_table, join_values in join_conditions.items():
+                for key, value in join_values.items():
+                    join_conditions_list.append(f"{key} = {value}")
+                query += f' INNER JOIN {join_table} ON {" AND ".join(join_conditions_list)}'
+        if conditions is not None:
+            for key, value in conditions.items():
+                conditions_list.append(f"{key} = '{value}'")
+            query += f' WHERE {" AND ".join(conditions_list)}'
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        if method == 'fetchone':
+            result = cursor.fetchone()
+            return result
+        elif method == 'fetchall':
+            result = cursor.fetchall()
+            return result
+
+    def commit(self, table_name: str, data: Dict[str, Any]):
+        columns = ', '.join(data.keys())
+        values = tuple(data.values())
+        query = f'INSERT INTO {table_name} ({columns}) VALUES {values}'
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
--- app.py
+++ app.py
@@ -1,34 +1,18 @@
-from flask import Flask, request
-from database_utils import SQLiteDatabase
+from flask import Flask, request, render_template
+from Project_utils import SQLiteDatabase
 
 app = Flask(__name__)
 
 
 @app.get('/')
 def index():
-    return 'Welcome to the main page'
+    return render_template('index.html', title='Главная')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def get_register_page():
     if request.method == 'GET':
-        return """
-        <form action="/register" method="post">
-            <label for="username">Имя пользователя:</label>
-            <input type="text" name="username" placeholder="Username"><br>
-            <br>
-            <label for="password">Пароль:</label>
-            <input type="password" name="password" placeholder="Password"><br>
-            <br>
-            <label for="birthday">Дата рождения:</label>
-            <input type="date" name='birthday' placeholder="Birthday"><br>
-            <br>
-            <label for="phone">Телефон:</label>
-            <input type="text" name='phone' placeholder="+380(__)___-__-__"><br>
-            <br>
-            <button type="submit">Регистрация</button>
-        </form>
-        """
+        return render_template('register.html', title='Регистрация')
     else:
         with SQLiteDatabase('base.db') as db:
             form_data = request.form
@@ -36,10 +20,7 @@ def get_register_page():
             password = form_data.get('password')
             birthday = form_data.get('birthday')
             phone = form_data.get('phone')
-            db.commit(
-                'INSERT INTO users (login, password, birth_date, phone) VALUES (?, ?, ?, ?)',
-                (user_name, password, birthday, phone)
-            )
+            db.commit('users', {'login': user_name, 'password': password, 'birth_date': birthday, 'phone': phone})
 
         return 'Registration success'
 
@@ -47,34 +28,26 @@ def get_register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def get_login_page():
     if request.method == 'GET':
-        return '''
-    <form action="/login" method="post">
-        <label for="username">Имя пользователя:</label>
-        <input type="text" name="username" placeholder="Username"><br>
-        <br>
-        <label for="password">Пароль:</label>
-        <input type="password" name="password" placeholder="Password"><br>
-        <br>
-        <button type="submit">Вход</button>
-    </form>
-    '''
+        return render_template('login.html', title='Авторизация')
     else:
         form_data = request.form
         user_name = form_data.get('username')
         password = form_data.get('password')
         with SQLiteDatabase('base.db') as db:
-            user = db.fetch_one('SELECT * FROM users WHERE login = ? AND password = ?', (user_name, password))
+            user = db.select(table_name='users', method='fetchone',
+                             conditions={'login': user_name, 'password': password})
             if user:
-                return 'Login success'
+                print(user)
+                return render_template('user.html', title='Личный кабинет', user=user)
             else:
                 return 'Login failed'
 
 
-@app.route('/user', methods=['GET', 'POST', 'PUT'])
-def get_user_page():
+@app.route('/user/<int:user_id>', methods=['GET', 'POST', 'PUT'])
+def get_user_page(user_id):
     if request.method == 'GET':
         with SQLiteDatabase('base.db') as db:
-            users = db.fetch_all('SELECT * FROM users')
+            users = db.select(table_name='users', method='fetchone', conditions={'id': user_id})
             return users
     if request.method == 'POST':
         return 'User created'
@@ -82,21 +55,27 @@ def get_user_page():
         return 'User updated'
 
 
-@app.route('/user/funds', methods=['GET', 'POST'])
-def get_funds_page():
+@app.route('/user/<int:user_id>/funds', methods=['GET', 'POST'])
+def get_funds_page(user_id):
     if request.method == 'GET':
         with SQLiteDatabase('base.db') as db:
-            funds = db.fetch_all('SELECT funds FROM users')
+            funds = db.select(table_name='users', method='fetchall', columns=['funds'], conditions={'id': user_id})
             return funds
     if request.method == 'POST':
         return 'Add something to funds'
 
 
-@app.route('/user/reservations', methods=['GET', 'POST', 'PUT'])
-def get_reservations_page():
+@app.route('/user/<int:user_id>/reservations', methods=['GET', 'POST', 'PUT'])
+def get_reservations_page(user_id):
     if request.method == 'GET':
         with SQLiteDatabase('base.db') as db:
-            reservations = db.fetch_all('SELECT * FROM reservations')
+            reservations = db.select(table_name='reservations', method='fetchall', conditions={'user_id': user_id},
+                                     columns=['reservations.id AS reservation_id', 'reservations.date',
+                                              'reservations.time',
+                                              'services.name AS service_name', 'users.login AS user_name'],
+                                     join_conditions={'users': {'reservations.user_id': 'users.id'}, 'services': {
+                                         'reservations.service_id': 'services.id'
+                                     }})
             if not reservations:
                 return 'Reservations not found'
             return reservations
@@ -110,7 +89,7 @@ def get_reservations_page():
 def get_reservation_page(reservation_id):
     if request.method == 'GET':
         with SQLiteDatabase('base.db') as db:
-            reservation = db.fetch_one('SELECT * FROM reservations WHERE id = ?', (reservation_id,))
+            reservation = db.select(table_name='reservations', method='fetchone', conditions={'id': reservation_id})
             if not reservation:
                 return 'Reservation not found'
             return reservation
@@ -130,19 +109,20 @@ def get_checkout_page():
         return 'Checkout updated'
 
 
-@app.get('/fitness_center')
+@app.get('/fitness_centers')
 def get_fitness_center_page():
     with SQLiteDatabase('base.db') as db:
-        fitness_centers = db.fetch_all('SELECT * FROM fitness_centers')
+        fitness_centers = db.select(table_name='fitness_centers', method='fetchall')
         if not fitness_centers:
             return 'Fitness centers not found'
-        return fitness_centers
+        return render_template('fitness_centers.html', title='Фитнес центры', fitness_centers=fitness_centers)
 
 
 @app.get('/fitness_center/<int:fitness_center_id>')
 def get_fitness_center_id_page(fitness_center_id):
     with SQLiteDatabase('base.db') as db:
-        fitness_center = db.fetch_one('SELECT * FROM fitness_centers WHERE id = ?', (fitness_center_id,))
+        fitness_center = db.select(table_name='fitness_centers', method='fetchone',
+                                   conditions={'id': fitness_center_id})
         if not fitness_center:
             return 'Fitness center not found'
         return fitness_center
@@ -151,17 +131,24 @@ def get_fitness_center_id_page(fitness_center_id):
 @app.get('/fitness_center/<int:fitness_center_id>/trainers')
 def get_trainer_page(fitness_center_id):
     with SQLiteDatabase('base.db') as db:
-        trainers = db.fetch_all('SELECT * FROM trainers WHERE fitness_center_id = ?', (fitness_center_id,))
+        trainers = db.select(table_name='trainers', method='fetchall',
+                             conditions={'trainers.fitness_center_id': fitness_center_id},
+                             columns=['trainers.id', 'trainers.name AS trainer_name', 'trainers.age', 'trainers.gender',
+                                      'fitness_centers.name AS fitness_center_name', 'trainers.fitness_center_id'],
+                             join_conditions={'fitness_centers': {'trainers.fitness_center_id': 'fitness_centers.id'}})
         if not trainers:
             return 'Trainers not found'
-        return trainers
+        return render_template('trainers.html', title='Тренеры', trainers=trainers)
 
 
 @app.get('/fitness_center/<int:fitness_center_id>/trainers/<int:trainer_id>')
 def get_trainer_id_page(fitness_center_id, trainer_id):
     with SQLiteDatabase('base.db') as db:
-        trainer = db.fetch_one('SELECT * FROM trainers WHERE fitness_center_id = ? AND id = ?',
-                               (fitness_center_id, trainer_id))
+        trainer = db.select(table_name='trainers', method='fetchone',
+                            conditions={'trainers.fitness_center_id': fitness_center_id, 'trainers.id': trainer_id},
+                            columns=['trainers.id', 'trainers.name AS trainer_name', 'trainers.age', 'trainers.gender',
+                                     'fitness_centers.name AS fitness_center_name'],
+                            join_conditions={'fitness_centers': {'trainers.fitness_center_id': 'fitness_centers.id'}})
         if not trainer:
             return 'Trainer not found'
         return trainer
@@ -169,23 +156,37 @@ def get_trainer_id_page(fitness_center_id, trainer_id):
 
 @app.route('/fitness_center/<int:fitness_center_id>/trainers/<int:trainer_id>/rating', methods=['GET', 'POST', 'PUT'])
 def get_trainer_rating_page(fitness_center_id, trainer_id):
-    if request.method == 'GET':
-        with SQLiteDatabase('base.db') as db:
-            rating = db.fetch_one('SELECT * from reviews WHERE trainer_id = ?',
-                                  (trainer_id,))
-            if not rating:
-                return 'Rating not found'
-            return rating
-    if request.method == 'POST':
-        return f'Rating of the trainer {trainer_id} created! Fitness center id: {fitness_center_id}'
-    if request.method == 'PUT':
-        return f'Rating of the trainer {trainer_id} updated! Fitness center id: {fitness_center_id}'
+    with SQLiteDatabase('base.db') as db:
+        if request.method == 'GET':
+            rating = db.select(table_name='reviews', method='fetchall',
+                               conditions={'trainer_id': trainer_id, 'fitness_center_id': fitness_center_id},
+                               columns=['trainers.name AS trainer_name', 'reviews.points', 'reviews.user_name',
+                                        'reviews.text'],
+                               join_conditions={'trainers': {'reviews.trainer_id': 'trainers.id'}}
+                               )
+            return render_template('rating.html', title='Рейтинг', rating=rating, fitness_center_id=fitness_center_id,
+                                   trainer_id=trainer_id)
+        if request.method == 'POST':
+            form_data = request.form
+            user_name = form_data.get('name')
+            points = form_data.get('points')
+            text = form_data.get('text')
+            db.commit('reviews', {'trainer_id': trainer_id, 'user_name': user_name, 'points': points, 'text': text})
+            return render_template('index.html')
+        if request.method == 'PUT':
+            return f'Rating of the trainer {trainer_id} updated! Fitness center id: {fitness_center_id}'
 
 
 @app.get('/fitness_center/<int:fitness_center_id>/services')
 def get_services_page(fitness_center_id):
     with SQLiteDatabase('base.db') as db:
-        services = db.fetch_all('SELECT * FROM services WHERE fitness_center_id = ?', (fitness_center_id,))
+        services = db.select(table_name='services', method='fetchall',
+                             conditions={'fitness_center_id': fitness_center_id},
+                             columns=['services.id AS service_id', 'services.name AS service_name',
+                                      'services.price',
+                                      'services.duration', 'fitness_centers.name AS fitness_center_name'],
+                             join_conditions={
+                                 'fitness_centers': {'services.fitness_center_id': 'fitness_centers.id'}})
         if not services:
             return 'Services not found'
         return services
@@ -194,8 +195,11 @@ def get_services_page(fitness_center_id):
 @app.get('/fitness_center/<int:fitness_center_id>/services/<int:service_id>')
 def get_service_id_page(fitness_center_id, service_id):
     with SQLiteDatabase('base.db') as db:
-        service = db.fetch_one('SELECT * FROM services WHERE fitness_center_id = ? AND id = ?',
-                               (fitness_center_id, service_id))
+        service = db.select(table_name='services', method='fetchone',
+                            conditions={'fitness_center_id': fitness_center_id, 'services.id': service_id},
+                            columns=['services.id AS service_id', 'services.name AS service_name', 'services.price',
+                                     'services.duration', 'fitness_centers.name AS fitness_center_name'],
+                            join_conditions={'fitness_centers': {'services.fitness_center_id': 'fitness_centers.id'}})
         if not service:
             return 'Service not found'
         return service
